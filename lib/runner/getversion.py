aede712f7d980f6763dfab7de9f58720d7c86758
Davide Tampellini <tampe125@gmail.com>
2016-11-03 23:53:28 +0100

    Added structure for 3 step version fingerprint

--- lib/runner/getversion.py
+++ lib/runner/getversion.py
@@ -13,8 +13,31 @@ class JScannerGetversion(AbstractCommand):
         self.parentArgs.url = self.parentArgs.url.strip('/') + '/'
 
     def run(self):
+        version = self._xml_file()
+
+        # If we can fetch the XML file, the version is 100% correct
+        if version:
+            return version
+
+        version = self._sql_files()
+
+        # If we have an exact match, let's return it
+        if len(version) == 1:
+            return version
+
+        # Still no version or more possible candidates? Time to fingerprint the media files
+        version = self._media_files(version)
+
+        return version
+    
+    def _xml_file(self):
+        return []
+
+    def _sql_files(self):
+        return []
+
+    def _media_files(self, version):
         base_url = self.parentArgs.url
-        version = []
 
         with open('data/hashes.json', 'rb') as json_handle:
             hashes = json.load(json_handle)
@@ -26,6 +49,9 @@ class JScannerGetversion(AbstractCommand):
             try:
                 candidates = signatures[digest]
             except KeyError:
+                # This should never happen, but better be safe than sorry
+                # TODO Warning style
+                print "Unknown %s signature for file %s" % (digest, filename)
                 continue
 
             if len(version) == 0:
@@ -33,9 +59,8 @@ class JScannerGetversion(AbstractCommand):
             else:
                 version = set(version).intersection(candidates)
 
+            # We have an exact match, we can stop here
             if len(version) == 1:
-                print "Version found: " + version.pop()
                 break
 
-        if len(version) != 1:
-            print "Possible versions found: " + ', '.join(version)
+        return version
